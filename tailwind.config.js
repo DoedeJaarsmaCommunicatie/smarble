7f6a47776969c65ecfe0fc4886da1f83360fdbf4
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2019-09-04 12:39:19 +0200

    Added dev files

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        ocean: {
+          default: 'hsl(197,100%,24%)'
+        },
+        sky: {
+          default: 'hsl(197,100%,43%)'
+        },
+        grass: {
+          default: 'hsl(156,100%,40%)'
+        }
+      }
+    },
+    fontFamily: {
+      default: [
+          'Open Sans'
+      ]
+    }
+  },
+  variants: {},
+  plugins: []
+}
